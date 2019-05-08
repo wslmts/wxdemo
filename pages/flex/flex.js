51f163ea9ee61f2cf7ad3d33727f62ea15a107b1
zhucuicui <zhucuicui@jd.com>
2019-05-08 10:21:24 +0800

    demo

--- pages/flex/flex.js
+++ pages/flex/flex.js
@@ -1,3 +1,29 @@
 Page({
-  
+  data:{
+    msg:''
+  },
+  onPullDownRefresh(){
+    this.msg="pull refresh"
+    console.log(123,arguments)
+  },
+  onReachBottom: function () {
+console.log(123)
+
+  },
+  clickMe(){
+    wx.showModal({
+      title: '显示提示信息',
+      content: '这是一个模态框，要查看提示信息的样子吗？',
+      success(res){
+        if(res.confirm){
+          wx.showToast({
+            title: '提示信息',
+            icon: 'success',
+            duration: 1000
+          })
+        } 
+      }
+    })
+    
+  }
 })
